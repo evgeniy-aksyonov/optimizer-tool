9e9b748b9c540c43c59cefc885a9eed41a3166b5
Evgeniy Aksyonov <fallenaxe2@yahoo.com>
2013-12-12 13:31:22 +0200

    Migrated from SQLite to Postgresql

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-School.create!(title: "Some High School")
 School.create!(title: "Mountain High School")
+School.create!(title: "Long Island High School")
 School.create!(title: "Another High School")
 School.create!(title: "Test High School")
+School.create!(title: "Some High School")
+School.create!(title: "Another One High School")
