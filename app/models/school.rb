e15d60502a7e66fb84e83526eb08e82c06499604
Evgeniy Aksyonov <fallenaxe2@yahoo.com>
2013-12-17 00:35:13 +0200

    Teachers model added

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,5 @@
 class School < ActiveRecord::Base
-	has_many :students
+	# has_many :students
+	# has_many :teachers
 	validates_presence_of :title
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216221922) do
+ActiveRecord::Schema.define(version: 20131216222407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,5 +31,13 @@ ActiveRecord::Schema.define(version: 20131216221922) do
   end
 
   add_index "students", ["school_id"], name: "index_students_on_school_id", using: :btree
+
+  create_table "teachers", force: true do |t|
+    t.integer  "school_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "teachers", ["school_id"], name: "index_teachers_on_school_id", using: :btree
 
 end
